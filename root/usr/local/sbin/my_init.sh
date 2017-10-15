9c7d1765d802e0ba35ea62aafee30fadeab73a08
Mark Stillwell <mark@stillwell.me>
2017-10-15 22:18:14 +0100

    fixup use of $config_file

--- root/usr/local/sbin/my_init.sh
+++ root/usr/local/sbin/my_init.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-log_dir=/var/log
-[ -L "$log_dir" ] && log_dir="$(readlink "$log_dir")"
-[ -d "$log_dir" ] || mkdir -m 0755 -p "$log_dir"
-
 if [ -d /etc/my_init.d ]; then
     find /etc/my_init.d -type f -maxdepth 1 | sort | while read SCRIPT; do
         if [ -x ${SCRIPT} ]; then
