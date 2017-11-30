5816ad930a36e69731d007ff7ae75596d5d38e5f
Mark Stillwell <mark@stillwell.me>
2017-11-30 22:04:20 +0000

    find wants -maxdepth before -type

--- root/usr/local/sbin/my_init.sh
+++ root/usr/local/sbin/my_init.sh
@@ -4,7 +4,7 @@ mkdir -m 1755 -p /tmp/run
 mkdir -m 1755 -p /tmp/run/lock
 
 if [ -d /etc/my_init.d ]; then
-    find /etc/my_init.d -type f -maxdepth 1 | sort | while read SCRIPT; do
+    find /etc/my_init.d -maxdepth 1 -type f | sort | while read SCRIPT; do
         if [ -x ${SCRIPT} ]; then
             ${SCRIPT}
         else
