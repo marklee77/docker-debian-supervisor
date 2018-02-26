54b0f735785216ac4339f0510a72a65fb70b32a8
Mark Stillwell <mark@stillwell.me>
2018-02-26 22:55:44 +0000

    catch up with alpine...

--- root/usr/local/share/my_init/functions.sh
+++ root/usr/local/share/my_init/functions.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-mypath() {
-    path="$1"
-    path="$(readlink "$path")"
-    parent="$(dirname "$path")"
-    saved_umask="$(umask)"
-    umask 022
-    mkdir -p "$parent"
-    umask "$saved_umask"
-    echo "$path"
-}
-
 openssl_ciphers() {
     tr -d '\n' <<EOF
 ECDHE-ECDSA-CHACHA20-POLY1305:
